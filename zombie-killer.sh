e840d96a7022748c8675a068bdb64db57a619f17
betodev <47487471+betoayza@users.noreply.github.com>
2023-07-11 20:52:07 -0300

    Adapted to english

--- zombie-killer.sh
+++ zombie-killer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Obtener una lista de todos los procesos zombie
+# zombie list processes
 zombies=$(ps aux | awk '{ if ($8 == "Z") { print $2 } }')
 
-# Matar cada proceso zombie
+# Kill each zombie process
 for zombie in $zombies; do
-    echo "Matando proceso zombie $zombie..."
+    echo "Killing zombie process: $zombie..."
     kill -s SIGCHLD $(ps -o ppid= -p $zombie)
 done
